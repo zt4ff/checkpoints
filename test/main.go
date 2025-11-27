f07d008e655f07dda8e91e715888013599294d02
olukayode <segunkayode00@gmail.com>
2025-11-27 17:02:35 +0100

    solution

--- test/main.go
+++ test/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	fmt.Print(piscine.FromTo(1, 10))
-	fmt.Print(piscine.FromTo(10, 1))
-	fmt.Print(piscine.FromTo(10, 10))
-	fmt.Print(piscine.FromTo(100, 10))
+	fmt.Println(piscine.Itoa(12345))
+	fmt.Println(piscine.Itoa(0))
+	fmt.Println(piscine.Itoa(-1234))
+	fmt.Println(piscine.Itoa(987654321))
 }
