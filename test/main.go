d8a51b32c35df39ecebe4fb3d9f8b2984cb6f3fb
olukayode <segunkayode00@gmail.com>
2025-11-27 13:31:48 +0100

    solution

--- test/main.go
+++ test/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	fmt.Print(piscine.FirstWord("this        ...       is sparta, then again, maybe    not"))
-	fmt.Print(piscine.FirstWord(" lorem,ipsum "))
-	fmt.Print(piscine.LastWord(" "))
+	fmt.Println(piscine.RepeatAlpha("abc"))
+	fmt.Println(piscine.RepeatAlpha("Choumi."))
+	fmt.Println(piscine.RepeatAlpha(""))
+	fmt.Println(piscine.RepeatAlpha("abacadaba 01!"))
 }
