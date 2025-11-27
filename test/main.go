b81fc0adf64a4c0d13a264c8ecf11a25ea666aaa
Kayode Oluwasegun <segunkayode00@gmail.com>
2025-11-27 22:51:26 +0100

    third time is a charm

--- test/main.go
+++ test/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"piscine"
 )
 
 func main() {
-	piscine.PrintRevComb()
+	fmt.Print(piscine.ThirdTimeIsACharm("123456789"))
+	fmt.Print(piscine.ThirdTimeIsACharm(""))
+	fmt.Print(piscine.ThirdTimeIsACharm("a b c d e f"))
+	fmt.Print(piscine.ThirdTimeIsACharm("12"))
 }
