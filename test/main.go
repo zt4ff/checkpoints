18ae7ed571433cbc7d136b62bb2b13153ff998b2
Kayode Oluwasegun <segunkayode00@gmail.com>
2025-11-27 21:40:22 +0100

    print mememory

--- test/main.go
+++ test/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.Itoa(12345))
-	fmt.Println(piscine.Itoa(0))
-	fmt.Println(piscine.Itoa(-1234))
-	fmt.Println(piscine.Itoa(987654321))
+	piscine.PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
 }
