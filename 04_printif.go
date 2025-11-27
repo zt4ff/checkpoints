0fe485eef7a25b26a76e4204a559611ae2b02268
olukayode <segunkayode00@gmail.com>
2025-11-27 09:28:45 +0100

    solution

---
+++ 04_printif.go
@@ -0,0 +1,9 @@
+package test
+
+func PrintIf(str string) string {
+	if len(str) >= 3 || len(str) == 0  {
+		return "G" + "\n"
+	}
+
+	return "Invalid Input" + "\n"
+}
--- test/main.go
+++ test/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main () {
-	fmt.Println(test.CountChar("Hello World", 'l'))
-	fmt.Println(test.CountChar("5  balloons", 5))
-	fmt.Println(test.CountChar("   ", ' '))
-	fmt.Println(test.CountChar("The 7 deadly sins", '7'))
+	fmt.Print(test.PrintIf("abcdefz"))
+	fmt.Print(test.PrintIf("abc"))
+	fmt.Print(test.PrintIf(""))
+	fmt.Print(test.PrintIf("14"))
 }
