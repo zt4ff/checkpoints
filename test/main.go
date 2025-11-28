ae0a5db971f8c7e4b5f8bf667e756bbbfffb0561
Kayode Oluwasegun <segunkayode00@gmail.com>
2025-11-28 06:27:14 +0100

    feat: wip

--- test/main.go
+++ test/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(piscine.ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
-	fmt.Println(piscine.ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
-	fmt.Println(piscine.ConcatAlternate([]int{1, 2, 3}, []int{}))
+	piscine.Inter()
 }
